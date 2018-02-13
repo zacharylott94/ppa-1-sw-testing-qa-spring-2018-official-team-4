8b878d5a8240ca07b151b72474988b941ae6b534
dcarley <dcarley@bomgar.com>
2018-02-12 23:43:17 -0600

    basic multiply function added

--- modules/basic.js
+++ modules/basic.js
@@ -7,5 +7,10 @@ var divide = (x,y) => {
     if(!y) throw new Error('Invalid dividend ' + y);
     return (x / y);
 }
-exports.divide = divide
+var multiply = (x,y) => {
+    // if(isNaN(x) || isNaN(y)) throw new TypeError("That isn't a number!");
+    return (x * y);
+}
 exports.add = add
+exports.divide = divide
+exports.multiply = multiply
