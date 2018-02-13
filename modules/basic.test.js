b5bc402455fe26fc8b0fc4eb87aef28699612fe1
dcarley <dcarley@bomgar.com>
2018-02-13 00:22:11 -0600

    Added fixed precision (1-4) to addition Corrected test descriptions for test cases Added fixed precision (1-4) to division Removed duplicate vars being used in multiplication

--- modules/basic.test.js
+++ modules/basic.test.js
@@ -5,11 +5,42 @@ const multiply = require('./basic').multiply
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+test('adds 1 + 0 to equal 1', () => {
+  expect(add(1, 0)).toBe(1);
+});
 test('adds -1 + 2 to equal 1', () => {
   expect(add(-1, 2)).toBe(1);
 });
-test('adds 1.2222222 + 2.222323232 to equal 3', () => {
+test('adds 1 + -2 to equal -1', () => {
+  expect(add(1, -2)).toBe(-1);
+});
+test('adds -1 + -2 to equal -3', () => {
+  expect(add(-1, -2)).toBe(-3);
+});
+
+test('adds 1.2222222 + 2.222323232 to equal 3.444545432', () => {
   expect(add(1.2222222, 2.222323232)).toBe(3.444545432);
+});
+//NOTE: toPrecision is a formatting function thus it will return the calculation as a string
+test('adds 1.2222222 + 2.222323232 to equal 3 Precision: 1', () => {
+  var num = add(1.2222222, 2.222323232)
+  var fpp = parseFloat(num).toPrecision(1)
+  expect(fpp).toBe("3");
+});
+test('adds 1.2222222 + 2.222323232 to equal 3.4 Precision: 2', () => {
+  var num = add(1.2222222, 2.222323232)
+  var fpp = parseFloat(num).toPrecision(2)
+  expect(fpp).toBe("3.4");
+});
+test('adds 1.2222222 + 2.222323232 to equal 3.44 Precision: 3', () => {
+  var num = add(1.2222222, 2.222323232)
+  var fpp = parseFloat(num).toPrecision(3)
+  expect(fpp).toBe("3.44");
+});
+test('adds 1.2222222 + 2.222323232 to equal 3.445 Precision: 4', () => {
+  var num = add(1.2222222, 2.222323232)
+  var fpp = parseFloat(num).toPrecision(4)
+  expect(fpp).toBe("3.445");
 });
 
 test('throws on string for 1st parameter', () => {
@@ -29,6 +60,11 @@ test('throws on string for both parameter', () => {
     add("string","string");
   }).toThrow();
 });
+test('throws on string for both parameter', () => {
+  expect(() => {
+    add("string","string");
+  }).toThrow();
+});
 ///End Addition 
 
 ///Division
@@ -36,20 +72,26 @@ test('throws on string for both parameter', () => {
 test('Divides 2 by 2 to get 1', () => {
   expect(divide(2, 2)).toBe(1);
 });
-test('Divides -2 by 2 to get 1', () => {
+test('Divides -2 by 2 to get -1', () => {
   expect(divide(-2, 2)).toBe(-1);
+});
+test('Divides 2 by -2 to get -1', () => {
+  expect(divide(2, -2)).toBe(-1);
 });
 test('Divides -2 by -2 to get 1', () => {
   expect(divide(-2, -2)).toBe(1);
 });
+test('Divides 5.3 by 2.7 to get 1.9629629629629628 ', () => {
+  expect(divide(5.3, 2.7)).toBe(1.9629629629629628);
+});
 
 //NOTE: toPrecision is a formatting function thus it will return the calculation as a string
-test('Divides 5.3 by 2.7 to get 1.96 Precision:1', () => {
+test('Divides 5.3 by 2.7 to get 2 Precision:1', () => {
   var num = divide(5.3, 2.7)
   var fpp = parseFloat(num).toPrecision(1)
   expect(fpp).toBe("2");
 });
-test('Divides 5.3 by 2.7 to get 1.96 Precision:2', () => {
+test('Divides 5.3 by 2.7 to get 2.0 Precision:2', () => {
   var num = divide(5.3, 2.7)
   var fpp = parseFloat(num).toPrecision(2)
   expect(fpp).toBe("2.0");
@@ -59,7 +101,7 @@ test('Divides 5.3 by 2.7 to get 1.96 Precision:3', () => {
   var fpp = parseFloat(num).toPrecision(3)
   expect(fpp).toBe("1.96");
 });
-test('Divides 5.3 by 2.7 to get 1.96 Precision:4', () => {
+test('Divides 5.3 by 2.7 to get 1.963 Precision:4', () => {
   var num = divide(5.3, 2.7)
   var fpp = parseFloat(num).toPrecision(4)
   expect(fpp).toBe( "1.963");
@@ -94,6 +136,12 @@ test('throws error on string for both parameters', () => {
 test('Multiply 3 * 3 to equal 9', () => {
   expect(multiply(3, 3)).toBe(9);
 });
+test('Multiply 999 * 0 to equal 0', () => {
+  expect(multiply(999, 0)).toBe(0);
+});
+test('Multiply 0 * 99999 to equal 0', () => {
+  expect(multiply(0, 99999)).toBe(0);
+});
 test('Multiply -4 * -4 to equal 16', () => {
   expect(multiply(-4, -4)).toBe(16);
 });
@@ -104,23 +152,23 @@ test('Multiply 4 * -4 to equal -16', () => {
   expect(multiply(4, -4)).toBe(-16);
 });
 //NOTE: toPrecision is a formatting function thus it will return the calculation as a string
-test('Multiply 3.7 * 4.9 to equal 18.13 Precision:1', () => {
+test('Multiply 3.7 * 4.9 to equal 2e+1 Precision:1', () => {
   var num = multiply(3.7, 4.9)
-  var fpp = parseFloat(multiply(3.7, 4.9)).toPrecision(1)
+  var fpp = parseFloat(num).toPrecision(1)
   expect(fpp).toBe("2e+1");
 });
-test('Multiply 3.7 * 4.9 to equal 18.13 Precision:2', () => {
+test('Multiply 3.7 * 4.9 to equal 18 Precision:2', () => {
   var num = multiply(3.7, 4.9)
-  var fpp = parseFloat(multiply(3.7, 4.9)).toPrecision(2)
+  var fpp = parseFloat(num).toPrecision(2)
   expect(fpp).toBe("18");
 });
-test('Multiply 3.7 * 4.9 to equal 18.13 Precision:3', () => {
+test('Multiply 3.7 * 4.9 to equal 18.1 Precision:3', () => {
   var num = multiply(3.7, 4.9)
-  var fpp = parseFloat(multiply(3.7, 4.9)).toPrecision(3)
+  var fpp = parseFloat(num).toPrecision(3)
   expect(fpp).toBe("18.1");
 });
 test('Multiply 3.7 * 4.9 to equal 18.13 Precision:4', () => {
   var num = multiply(3.7, 4.9)
-  var fpp = parseFloat(multiply(3.7, 4.9)).toPrecision(4)
+  var fpp = parseFloat(num).toPrecision(4)
   expect(fpp).toBe("18.13");
 });
