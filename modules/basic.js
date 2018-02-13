2d68e7bf5974ec9a311a028ad78e0356c8758ca3
dcarley <dcarley@bomgar.com>
2018-02-12 23:29:24 -0600

    added division function

--- modules/basic.js
+++ modules/basic.js
@@ -2,4 +2,10 @@ var add = (x,y) => {
     if(isNaN(x) || isNaN(y)) throw new TypeError("That isn't a number!");
     return x + y;
     }
+var divide = (x,y) => {
+    if(isNaN(x) || isNaN(y)) throw new TypeError("That isn't a number!");
+    if(!y) throw new Error('Invalid dividend ' + y);
+    return (x / y);
+}
+exports.divide = divide
 exports.add = add
