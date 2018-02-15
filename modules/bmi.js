a15f24b4e3064dd00f310673109e734dcaee6796
dcarley <dcarley@bomgar.com>
2018-02-14 21:56:56 -0600

    included required modules and made bmi function

--- modules/bmi.js
+++ modules/bmi.js
@@ -1,1 +1,16 @@
-const add = require('./basic_modules').add
+const add = require('./basic_modules/add').add
+const divide= require('./basic_modules/divide').divide
+const multiply = require('./basic_modules/multiply').multiply
+
+const MCFPOUNDS = 0.45
+
+//Multiply the weight in pounds by MCF
+//Height and Weight are not ready to use as is
+
+//todo test numbers for bmi
+var bmi = (height, weight) => {
+     if(isNaN(x) || isNaN(y)) throw new TypeError("That isn't a number!");
+     
+     var weightToPounds = multiply(weight, MCFPOUNDS)
+
+    }
