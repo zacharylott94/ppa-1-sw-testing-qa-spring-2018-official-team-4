1da5bc09a0de6737f6e3d77e7815a627900695ca
Zach Lott <zacharylott94@gmail.com>
2018-02-15 16:11:07 -0600

    Made yearlySavings.js and retirement.js utilize arithmetic functions

--- modules/basic_modules/yearlySavings.js
+++ modules/basic_modules/yearlySavings.js
@@ -1,8 +1,8 @@
 const verifyNumbers = require("./verifyNumbers.js")
-
+const multiply = require("./multiply").multiply
 function yearlySavings(salary, rate){
   if (!verifyNumbers(arguments)) {throw new TypeError("Numbers Only")}
-  return (salary * rate)
+  return multiply(salary,rate)
 }
 
 module.exports = yearlySavings
--- modules/retirement.js
+++ modules/retirement.js
@@ -1,12 +1,16 @@
 const modules = "./basic_modules/"
 const verifyNumbers = require(modules + "verifyNumbers.js")
 const yearlySavings = require(modules + "yearlySavings")
+const divide = require(modules + "divide").divide
+const add = require(modules + "add").add
+const multiply = require(modules + "multiply").multiply
+
 function retirement(age, salary, percent, goal) {
   if (!verifyNumbers(arguments)) {throw new TypeError("Numbers Only")}
-  let adjustedRate = percent + (percent * .35)
-  let years = goal / yearlySavings(salary,adjustedRate)
+  let adjustedRate = add(percent,(multiply(percent,.35)))
+  let years = divide(goal,yearlySavings(salary,adjustedRate))
   years = Math.ceil(years)
-  let answer = years + age
+  let answer = add(years,age)
   if (answer > 99) {return "Savings goal not met."}
   return answer
 }
