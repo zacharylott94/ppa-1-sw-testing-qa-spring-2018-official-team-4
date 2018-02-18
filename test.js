4841d222ff28be244b8ca1aa750557c810ea969a
dcarley <dcarley@bomgar.com>
2018-02-17 23:40:51 -0600

    repl with inquirer

---
+++ test.js
@@ -0,0 +1,79 @@
+/**
+ * Recursive prompt example
+ * Allows user to choose when to exit prompt
+ */
+
+'use strict';
+var inquirer = require('./node_modules/inquirer');
+var bmi = require('./modules/bmi').bmi
+var output = [];
+
+//where your questions would go
+var bmiQuestions = [{
+    
+    type: 'input',
+    name: 'userHeight',
+    message: "What's your height? " + "5'" +'11"'
+  
+},{
+    
+    type: 'input',
+    name: 'userWeight',
+    message: "What's your weight?"
+  
+}
+];
+//ask to exit prompt
+var askToExit = [{
+    type: 'confirm',
+    name: 'askAgain',
+    message: 'Would you want to use another tool? (just hit enter for YES)?',
+    default: true
+  }]
+
+  //general questions
+var questions = [
+    {
+        type: 'list',
+        name: 'theme',
+        message: 'What would you like to use?',
+        choices: [
+            "1. BMI Calculator 💪",
+            "2. Shortest Distance Calculator 🏃",
+            "3. Email verifier ✉️",
+            "4. Split the Tip Calculator 💵",
+            "5. Retirement Calculator 👵",
+          new inquirer.Separator(),
+          'Exit'
+        ]
+      }
+  
+];
+
+function ask() {
+  inquirer.prompt(questions).then(answers => {
+    output.push(answers.theme);
+    if(answers.theme == "1. BMI Calculator 💪"){
+        askBmi()
+    }
+    
+  });
+}
+function askExit() {
+    inquirer.prompt(askToExit).then(answers => {
+     if(answers.askAgain){
+         ask()
+     }
+      
+      
+    });
+  }
+
+function askBmi(){
+    inquirer.prompt(bmiQuestions).then(answers => {
+        console.log(bmi(answers.userHeight,answers.userWeight));
+        askExit();
+      });
+}
+
+ask();
