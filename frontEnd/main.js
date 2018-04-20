896223690c81750ea292d9fb5ddaf2b5e5b5722f
Zach Lott <zacharylott94@gmail.com>
2018-04-20 14:31:00 -0500

    Retirement encapsulated into Vue component

--- frontEnd/main.js
+++ frontEnd/main.js
@@ -1,4 +1,43 @@
 //This does Vue stuff
+Vue.component('retirement',{
+  template: `
+  <div>
+    <h3>Retirement Calculator</h3>
+    <input type="text" placeholder="Age" v-model='age'>
+    <input type="text" placeholder="Salary" v-model='salary'>
+    <input type="text" placeholder="Interest Rate" v-model='rate'>
+    <input type="text" placeholder="Investment Goal" v-model='goal'>
+    <p>Retirement Age: {{result}}</p>
+    <button @click='calculateRetirement'>Calculate!</button>
+  </div>
+  `,
+  data() {
+    return {
+      age: '',
+      salary: '',
+      rate: '',
+      goal: '',
+      result: ''
+    }
+  },
+  methods: {
+    calculateRetirement() {
+      let age = Number(this.age)
+      let salary = Number(this.salary)
+      let rate = Number(this.rate)
+      let goal = Number(this.goal)
+      try {
+        this.result = bundle.retirement(age, salary, rate, goal)
+      }
+      catch (err) {
+        this.result = String(err)
+      }
+    }
+  }
+})
+
+
+
 new Vue({
   el:'#root',
   data: {
@@ -11,19 +50,6 @@ new Vue({
     }
   },
   methods: {
-    calculateRetirement() {
-      let age = Number(this.retirement.age)
-      let salary = Number(this.retirement.salary)
-      let rate = Number(this.retirement.rate)
-      let goal = Number(this.retirement.goal)
-      console.log(age)
-      try {
-        this.retirement.result = bundle.retirement(age, salary, rate, goal)
-      }
-      catch (err) {
-        console.log(err)
-        this.retirement.result = String(err)
-      }
-    }
+
   }
 })
