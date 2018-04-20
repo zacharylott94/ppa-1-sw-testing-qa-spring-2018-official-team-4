056b30618b994eee14706dc3bd6b794bcc6b54f8
Zach Lott <zacharylott94@gmail.com>
2018-04-20 14:54:20 -0500

    BMI component implemented

--- frontEnd/main.js
+++ frontEnd/main.js
@@ -1,15 +1,15 @@
 //This does Vue stuff
 Vue.component('retirement',{
   template: `
-  <div>
-    <h3>Retirement Calculator</h3>
-    <input type="text" placeholder="Age" v-model='age'>
-    <input type="text" placeholder="Salary" v-model='salary'>
-    <input type="text" placeholder="Interest Rate" v-model='rate'>
-    <input type="text" placeholder="Investment Goal" v-model='goal'>
-    <p>Retirement Age: {{result}}</p>
-    <button @click='calculateRetirement'>Calculate!</button>
-  </div>
+    <div>
+      <h3>Retirement Calculator</h3>
+      <input type="text" placeholder="Age" v-model='age'>
+      <input type="text" placeholder="Salary" v-model='salary'>
+      <input type="text" placeholder="Interest Rate" v-model='rate'>
+      <input type="text" placeholder="Investment Goal" v-model='goal'>
+      <p>Retirement Age: {{result}}</p>
+      <button @click='calculateRetirement'>Calculate!</button>
+    </div>
   `,
   data() {
     return {
@@ -36,6 +36,34 @@ Vue.component('retirement',{
   }
 })
 
+Vue.component('bmi',{
+  template:`
+    <div>
+      <h3>BMI Calculator</h3>
+      <input type="text" placeholder="height" v-model='height'>
+      <input type="text" placeholder="weight" v-model='weight'>
+      <p>Result: {{result}}</p>
+      <button @click=calculateBMI>Calculate</button>
+      </div>
+  `,
+  data() {
+    return {
+      height:'',
+      weight:'',
+      result:''
+    }
+  },
+  methods: {
+    calculateBMI() {
+      try {
+        this.result = bundle.bmi(this.height,Number(this.weight))
+      }
+      catch(err) {
+        this.result = String(err)
+      }
+    }
+  }
+})
 
 
 new Vue({
