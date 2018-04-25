b33c38ba5b93f89e3fb17592def23208c2f1309f
dcarley <dcarley@bomgar.com>
2018-04-25 17:09:15 -0500

    added vue export

--- imports.js
+++ imports.js
@@ -1,5 +1,4 @@
 module.exports.Vue = require('./node_modules/vue/dist/vue.js')
-
 module.exports.bmi = require('./modules/bmi').bmi
 module.exports.splitTip = require('./modules/splitTip').splitTip
 module.exports.distance = require('./modules/distance').distance
